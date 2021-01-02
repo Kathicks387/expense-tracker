28009f74af102da18785b3666de04855ac41b102
Katrina Hicks <Katrinarhicks@gmail.com>
2021-01-02 13:10:06 -0500

    added to js file

--- script.js
+++ script.js
@@ -15,6 +15,35 @@ const dummyTransactions = [
 
 let transactions = dummyTransactions;
 
+// Add transaction
+function addTransaction(e) {
+    e.preventDefault();
+
+    if(text.value.trim() === '' || amount.value.trim() === '') {
+        alert('Please add a text and amount');
+    } else {
+        const transaction = {
+          id: generateID(),
+          text: text.value,
+          amount: +amount.value
+        };
+
+        transactions.push(transaction);
+
+        addTransactionDOM(transaction);
+
+        updateValues();
+
+        text.value = '';
+        amount.value = '';
+    }
+}
+
+// Generate random ID
+function generateID() {
+    return Math.floor(Math.random() * 1000000000);
+}
+
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
     // Get sign
@@ -26,11 +55,43 @@ function addTransactionDOM(transaction) {
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
     item.innerHTML = `
-    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
-    <button class='delete-btn'>x</button>
+      ${transaction.text} <span>${sign}${Math.abs(
+      transaction.amount
+    )}</span> <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
     `;
 
     list.appendChild(item);
+  }
+
+  // Update the balance, income and expense
+  function updateValues() {
+     const amounts = transactions.map(transaction =>
+        transaction.amount);
+
+     const total = amounts.reduce((acc, item) => (acc += item), 0)
+     .toFixed(2);
+
+     const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+     const expense = amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => (acc += item), 0) * -1
+        .toFixed(2);
+
+     balance.innerText = `$${total}`;
+     money_plus.innerText = `$${income}`;
+     money_minus.innerText = `$${expense}`;
+  }
+
+  // Remove transaction by ID
+  function removeTransaction(id) {
+      transactions = transactions.filter(transaction => transaction.id
+      !== id);
+
+      init();
   }
 
   // Init app
@@ -38,6 +99,9 @@ function addTransactionDOM(transaction) {
       list.innerHTML = '';
 
       transactions.forEach(addTransactionDOM);
+      updateValues();
   }
 
   init();
+
+form.addEventListener('submit', addTransaction); 
